70c747a4dbb11bf609551779704864f2e54674b2
Wimmu <wimmu.isberg@gmail.com>
2023-01-11 22:47:24 +0200

    Tehtävä 1

---
+++ 04_suorakulmio.py
@@ -0,0 +1,8 @@
+kanta = float(input("Kerro suorakulmion kanta:"))
+korkeus = float(input("Kerro suorakulmion korkeus:"))
+
+piiri = kanta+kanta+korkeus+korkeus
+ala = kanta*korkeus
+
+print("Suorakulmion piiri on:", piiri)
+print("Suorakulmion pinta-ala on:", ala)
---
+++ 06_keskiaikaisetmitat.py
@@ -0,0 +1,10 @@
+leiviskä = float(input("Anna leiviskät:"))
+naula = float(input("Anna luodit:"))
+luoti = float(input("Anna naulat:"))
+
+g_luoti = luoti*13.3
+
+
+print(g_luoti)
+
+????????
