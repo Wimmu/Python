1f99ebc08ab7d06a9e09ca4911d21ed6bbe77b1d
Wimmu <wimmu.isberg@gmail.com>
2023-01-11 22:25:56 +0200

    Tehtävä 1

---
+++ 03_pinta-ala.py
@@ -0,0 +1,7 @@
+import math as M
+
+säde = float(input("Kerro ympyrän säde:"))
+
+ala = M.pi*säde**2
+
+print("Ympyrän pinta-ala on", ala)
---
+++ 05_summatulokeskiarvo.py
@@ -0,0 +1,11 @@
+num1 = float(input("Anna ensimmäinen numero:"))
+num2 = float(input("Anna toinen numero:"))
+num3 = float(input("Anna kolmas numero:"))
+
+summa = num1+num2+num3
+tulo = num1*num2*num3
+keskiarvo = (num1+num2+num3)/3
+
+print("Lukujen summa on:", summa)
+print("Lukujen tulo on:", tulo)
+print("Lukujen keskiarvo on:", keskiarvo)
