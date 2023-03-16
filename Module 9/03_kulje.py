61e9d896aa4e866ca2b0d3b8d4ed5ed28002b62c
Wimmu <wimmu.isberg@gmail.com>
2023-03-16 13:43:45 +0200

    :)

---
+++ Module 9/03_kulje.py
@@ -0,0 +1,23 @@
+class Auto:
+    def __init__(self, rekisteritunnus, huippunopeus):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.nopeus = 0
+        self.kuljettumatka = 0
+
+    def kiihdyntä(self, nopeudenmuutos):
+        self.nopeus += nopeudenmuutos
+        if self.nopeus > self.huippunopeus:
+            self.nopeus = self.huippunopeus
+        if self.nopeus < 0:
+            self.nopeus = 0
+
+    def kulje(self, tuntimäärä):
+        self.kuljettumatka = self.kuljettumatka + self.nopeus*tuntimäärä
+
+mersu = Auto("ABC-123", 142)
+
+mersu.kiihdyntä(30)
+mersu.kulje(1.5)
+
+print(mersu.kuljettumatka)
