2a10bb2721c5b809b4f6336d781258d6e0bf0680
Wimmu <wimmu.isberg@gmail.com>
2023-03-27 09:52:12 +0300

    :)

--- Module 12/01_chuck.py
+++ Module 12/01_chuck.py
@@ -1,11 +1,11 @@
 import requests
 import json
 
-# https://api.tvmaze.com/search/shows?q= on JSON jonka palveluntarjoaja päättää itse
-response = requests.get('https://api.chuchnorris.io/jokes/random')
-data = json.loads(response.text)
-joke = data['Value']
+pyyntö = "https://api.chuchnorris.io/jokes/random?"
 
-print(joke)
+vastaus = requests.get(pyyntö)
+json_vastaus = vastaus.json()
+
+print(json_vastaus["value"])
 
 
