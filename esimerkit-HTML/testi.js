fc7a8f6e1728c2158a80baac433f029eb0700ceb
Wimmu <wimmu.isberg@gmail.com>
2023-04-06 08:48:57 +0300

    :)

--- esimerkit-HTML/testi.js
+++ esimerkit-HTML/testi.js
@@ -1,5 +1,9 @@
-
 'use strict';
 console.log("Moroooo");
 const name = prompt('Syötä nimesi:');
 document.querySelector('#target').innerHTML = 'Huomenta, ' + name + '!';
+
+const names = ['Silva', 'Veeti', 'Wimmu'];
+for(let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
